5144038a54fddceb85b83096115090a360a9c6c7
Andersonb7369 <andersonb7369@gmail.com>
2025-03-31 22:35:19 -0300

    Excercicios Logico

---
+++ Mini Projects/1-DesafioSuperTrunfo/CartaTrunfoLogica.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ESTADOS 1
+#define CIDADES 2
+
+typedef struct {
+    int numeroCarta;
+    char estado;
+    char codigo[4];
+    char nomeCidade[25];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTurusticos;
+    float densidade;
+    float pibPercapita;
+    float superPoder;
+} Trunfo;
+
+typedef struct {
+    int Cartas[6];
+} Cartas;
+
+// função cadastrar carta
+void cadastrar(Trunfo cartaTrunfo[ESTADOS][CIDADES]) {
+    char estados[] = "ABCDEFGH"; // cria as letras das cartas(estados)
+    int ncarta = 1;
+    for (int e = 0; e < ESTADOS; e++ ){  
+        for (int c = 0; c < CIDADES; c++){  // itera sobre os estados e cartaTrunfo gerando o codigo da carta'
+            
+            cartaTrunfo[e][c].numeroCarta = ncarta;
+            cartaTrunfo[e][c].estado = estados[e];
+
+            printf("Codigo %c%02d:\n", estados[e], c + 1);
+            sprintf(cartaTrunfo[e][c].codigo, "%c%02d", estados[e], c + 1);
+
+            printf("Nome da Cidade: ");
+            scanf("%s", &cartaTrunfo[e][c].nomeCidade); // aceita apenas nomes sem espaços
+
+            // fgets(cartaTrunfo[e][c].nomeCidade, sizeof(cartaTrunfo[e][c].nomeCidade), stdin); //captura nome com espaço
+            // cartaTrunfo[e][c].nomeCidade[strcspn(cartaTrunfo[e][c].nomeCidade, "\n")] = '\0'; //remove o \n do final da linha
+            
+            printf("População: ");
+            scanf("%d", &cartaTrunfo[e][c].populacao);
+
+            printf("Área Mt2: ");
+            scanf("%f", &cartaTrunfo[e][c].area);
+
+            printf("PIB (milhoes): ");
+            scanf("%f", &cartaTrunfo[e][c].pib);
+
+            printf("Número de pontos turísticos: ");
+            scanf("%d", &cartaTrunfo[e][c].pontosTurusticos);
+
+            // calcula a densodade e armazena na variavel
+            cartaTrunfo[e][c].densidade = cartaTrunfo[e][c].populacao / cartaTrunfo[e][c].area;
+            // calcula o pibPercapta
+            cartaTrunfo[e][c].pibPercapita = (cartaTrunfo[e][c].pib * 1000)/ cartaTrunfo[e][c].populacao;
+            // calcula o super poder
+            cartaTrunfo[e][c].superPoder =  cartaTrunfo[e][c].populacao + 
+                                            cartaTrunfo[e][c].area +
+                                            cartaTrunfo[e][c].pib * 1000 +
+                                            cartaTrunfo[e][c].pontosTurusticos +
+                                            cartaTrunfo[e][c].pibPercapita +
+                                            cartaTrunfo[e][c].densidade;            
+            printf("\n");
+            ncarta++;
+        }
+    }
+}
+
+
+// exibe as cartas após o cadastro
+void exibirCartas(Trunfo cartaTrunfo[ESTADOS][CIDADES]){
+    for (int e = 0; e < ESTADOS; e++ ){  
+        for (int c = 0; c < CIDADES; c++){
+            printf("\nCarta %d:\n", cartaTrunfo[e][c].numeroCarta);
+            printf("Estado: %c\n", cartaTrunfo[e][c].estado);
+            printf("Codigo: %s\n", cartaTrunfo[e][c].codigo);
+            printf("Nome da Cidade: %s\n", cartaTrunfo[e][c].nomeCidade);
+            printf("População: %d\n", cartaTrunfo[e][c].populacao);
+            printf("Área: %.2f Km2\n", cartaTrunfo[e][c].area);
+            printf("PIB: %.2f bilhoes de reais\n", cartaTrunfo[e][c].pib/1000);
+            printf("Pontos Tur: %d\n", cartaTrunfo[e][c].pontosTurusticos);
+            printf("Densidade: %.2f hab/km2\n", cartaTrunfo[e][c].densidade);
+            printf("Pib PerCapita: %.2f reais\n", cartaTrunfo[e][c].pibPercapita);
+            printf("Super poder: %.0f\n\n", cartaTrunfo[e][c].superPoder);
+            printf("--------------------------------------------------");
+            
+        }
+    }
+}
+
+
+// comparar todas itens das cartas
+void compararCartas(Trunfo carta1, Trunfo carta2) {
+    int populacao1 = carta1.populacao;
+    int populacao2 = carta2.populacao;
+    int area1 = carta1.area;
+    int area2 = carta2.area;
+    int pib1 = carta1.pib;
+    int pib2 = carta2.pib;
+    int pontoTur1 = carta1.pontosTurusticos;
+    int pontoTur2 = carta2.pontosTurusticos;
+    int densidade1 = carta1.densidade;
+    int densidade2 = carta2.densidade;
+    int pibPerCapta1 = carta1.pibPercapita;
+    int pibPerCapta2 = carta2.pibPercapita;
+    int superPoder1 = carta1.superPoder;
+    int superPoder2 = carta2.superPoder;
+
+    printf("\nComparando cartas: %s e %s\n\n", carta1.codigo, carta2.codigo);
+
+    if (populacao1 > populacao2){
+        printf("População: Carta 1 Venceu");
+    } else if (populacao1 < populacao2) {
+        printf("População: Carta 2 Venceu");
+    } else {
+        printf("População: Empate");
+    }
+
+    if (area1 > area2) {
+        printf("\nÁrea: Carta 1 Venceu");
+    } else if (area1 < area2) {
+        printf("\nÁrea: Carta 2 Venceu");
+    } else {
+        printf("\nÁrea: Empate");
+    }
+
+    if (pib1 > pib2) {
+        printf("\nPIB: Carta 1 Venceu");
+    } else if (pib1 < pib2) {
+        printf("\nPIB: Carta 2 Venceu");
+    } else {
+        printf("\nPIB: Empate");
+    }
+
+    if (pontoTur1 > pontoTur2) {
+        printf("\nPontos Turísticos: Carta 1 Venceu");
+    } else if (pontoTur1 < pontoTur2) {
+        printf("\nPontos Turísticos: Carta 2 Venceu");
+    } else {
+        printf("\nPontos Turísticos: Empate");
+    }
+
+    if (densidade1 > densidade2) {
+        printf("\nDensidade: Carta 1 Venceu");
+    } else if (densidade1 < densidade2) {
+        printf("\nDensidade: Carta 2 Venceu");
+    } else {
+        printf("\nDensidade: Empate");
+    }
+
+    if (pibPerCapta1 < pibPerCapta2) {
+        printf("\nPIB Per Capita: Carta 1 Venceu");
+    } else if (pibPerCapta1 > pibPerCapta2) {
+        printf("\nPIB Per Capita: Carta 2 Venceu");
+    } else {
+        printf("\nPIB Per Capita: Empate");
+    }
+
+    if (superPoder1 > superPoder2) {
+        printf("\nSuper Poder: Carta 1 Venceu");
+    } else if (superPoder1 < superPoder2) {
+        printf("\nSuper Poder: Carta 2 Venceu");
+    } else {
+        printf("\nSuper Poder: Empate");
+    }
+}
+
+// comparar apenas 1 iten das cartas
+void compararUmItem(Trunfo carta1, Trunfo carta2) {
+    int populacao1 = carta1.populacao;
+    int populacao2 = carta2.populacao;
+    
+    printf("\nComparação de cartas (Atributo: População):\n"); 
+    printf("Carta 1 - %s: %d\n", carta1.nomeCidade, carta1.populacao);
+    printf("Carta 2 - %s: %d\n", carta2.nomeCidade, carta2.populacao);
+    
+    if (populacao1 > populacao2){
+        printf("Resultado: Carta 1 (%s) Venceu", carta1.nomeCidade);
+    } else if (populacao1 < populacao2) {
+        printf("Resultado: Carta 2 (%s) Venceu", carta2.nomeCidade);
+    } else {
+        printf("Resultado: Empate");
+    }
+}
+
+int main() {
+    Trunfo cartaTrunfo[ESTADOS][CIDADES];
+    cadastrar(cartaTrunfo);
+    exibirCartas(cartaTrunfo);
+    compararUmItem(cartaTrunfo[0][0], cartaTrunfo[0][1]);
+}
+
+
+
+
--- Mini Projects/1-DesafioSuperTrunfo/novo.c
+++ Mini Projects/1-DesafioSuperTrunfo/novo.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// função int retorna algo
+int compara(int numero1, int numero2){
+    if (numero1 > numero2){
+        printf("Numero 1 (%d) é maior do que o numero 2 (%d)\n", numero1, numero2);
+    } else if (numero1 < numero2) {
+        printf("Numero 2 (%d) é maior do que o numero 1 (%d)\n", numero2, numero1);
+    } else {
+        printf("Os numeros são iguais\n");
+    }
+    return 0;
+}
+
+// função int retorna algo
+int compararr(int numero1, int numero2){
+    if (numero1 > numero2){
+        return 1;
+    } else if (numero1 < numero2) {
+        return -1;   
+    } else if (numero1 = numero2) {
+        return 2;
+    }
+    return 0;
+}
+
+// função void nao tem retorno de valores
+void comparar(int numero1, int numero2){
+    if (numero1 > numero2){
+        printf("Numero 1 (%d) é maior do que o numero 2 (%d)\n", numero1, numero2);
+    } else if (numero1 < numero2) {
+        printf("Numero 2 (%d) é maior do que o numero 1 (%d)\n", numero2, numero1);
+    } else {
+        printf("Os numeros são iguais\n");
+    }
+}
+
+
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // fixa os valores das variaveis
+    int numero1 = 10;
+    int numero2 = 25;
+
+    // solicita os valores para o usuario
+    printf("Digite 2 numeros com espaço entre eles: ");
+    scanf("%d %d", &numero1, &numero2);
+
+    compara(numero1, numero2);
+    comparar(numero1, numero2);
+
+    int resultado = compararr(numero1, numero2);
+    printf("Resultado: %d\n", resultado);
+
+    if (resultado = 1){
+        printf("Numero 1 (%d) é maior do que o numero 2 (%d)\n", numero1, numero2);
+    } else if (resultado = -1) {
+        printf("Numero 2 (%d) é maior do que o numero 1 (%d)\n", numero2, numero1);
+    } else {
+        printf("Os numeros são iguais\n");
+    }
+
+
+    //compara quem é o numero maior
+    // if (numero1 > numero2){
+    //     printf("Numero 1 (%d) é maior do que o numero 2 (%d)", numero1, numero2);
+    // } else {
+    //     printf("Numero 2 (%d) é maior do que o numero 1 (%d)", numero2, numero1);
+    // }
 
     return 0;
 }
